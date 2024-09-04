0d884158bb9ecf868562691a1d6f372869f10c78
Aidan <aidanwebster987@gmail.com>
2024-09-04 01:45:14 -0500

    Fixed all the errors, cube is rendering and rotating

--- webgpu-cube/src/main.ts
+++ webgpu-cube/src/main.ts
@@ -1,239 +1,191 @@
+import { mat4, vec3 } from 'wgpu-matrix';
+
 import vertexShader from './shaders/vertex.wgsl?raw';
 import fragmentShader from './shaders/fragment.wgsl?raw';
-import computeShader from './shaders/compute.wgsl?raw';
 
-main()
+import { cubeVertexArray, cubeVertexCount } from './cube';
+import { cubeVertexSize } from './cube';
+import { cubePositionOffset } from './cube';
+import { cubeUVOffset } from './cube';
 
-export async function main() {
-    const GRID_SIZE = 32;
-    const UPDATE_INTERVAL = 200;
-    const WORKGROUP_SIZSE = 8;
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const adapter = await navigator.gpu?.requestAdapter();
+const device = await adapter?.requestDevice();
 
-    const canvas = document.querySelector("canvas");
-
-    // Your WebGPU code will begin here!
-    if (!navigator.gpu) {
-        throw new Error("WebGPU is not supported on this browser.");
-    }
-
-    const adapter = await navigator.gpu.requestAdapter();
-    if (!adapter) {
-        throw new Error("No appropriate GPUAdapter found.");
-    }
-
-    if (!canvas) {
-        throw new Error("Problem fetching canvas");
-    }
-
-    const device = await adapter.requestDevice();
-    const context = canvas.getContext("webgpu");
-    const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-
-    if (!context) {
-        throw new Error("Probvlem fetching context");
-    }
-
-    context.configure({
-        device: device,
-        format: canvasFormat
-    });
-
-    const vertices = new Float32Array([
-        //    X     Y
-        -0.8, -0.8,
-        0.8, -0.8, // Triangle 1
-        0.8, 0.8,
-
-        -0.8, -0.8,
-        0.8, 0.8, // Triangle 2: note that ordering of the vertices DOES matter
-        -0.8, 0.8,
-    ]);
-
-    const vertexBuffer = device.createBuffer({
-        label: "Cell vertices",
-        size: vertices.byteLength,
-        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-    });
-    device.queue.writeBuffer(vertexBuffer, 0, vertices);
-
-    const vertexBufferLayout: GPUVertexBufferLayout = {
-        arrayStride: 8,
-        attributes: [{
-            format: "float32x2",
-            offset: 0,
-            shaderLocation: 0
-        }]
-    };
-
-    // Create the bind group layout and pipeline layout.
-    const bindGroupLayout = device.createBindGroupLayout({
-        label: "Cell Bind Group Layout",
-        entries: [{
-            binding: 0,
-            // Add GPUShaderStage.FRAGMENT here if you are using the `grid` uniform in the fragment shader.
-            visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE | GPUShaderStage.FRAGMENT,
-            buffer: {} // Grid uniform buffer
-        }, {
-            binding: 1,
-            visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
-            buffer: { type: "read-only-storage" } // Cell state input buffer
-        }, {
-            binding: 2,
-            visibility: GPUShaderStage.COMPUTE,
-            buffer: { type: "storage" } // Cell state output buffer
-        }]
-    });
-
-    const pipelineLayout = device.createPipelineLayout({
-        label: "Cell Pipeline Layout",
-        bindGroupLayouts: [bindGroupLayout],
-    });
-
-    const cellShaderModule = device.createShaderModule({
-        label: "Cell shader",
-        code: vertexShader + fragmentShader
-    });
-
-    // Create a pipeline that renders the cell.
-    const cellPipeline = device.createRenderPipeline({
-        label: "Cell pipeline",
-        layout: pipelineLayout,
-        vertex: {
-            module: cellShaderModule,
-            entryPoint: "vertexMain",
-            buffers: [vertexBufferLayout]
-        },
-        fragment: {
-            module: cellShaderModule,
-            entryPoint: "fragmentMain",
-            targets: [{
-                format: canvasFormat
-            }]
-        }
-    });
-
-    const simulationShaderModule = device.createShaderModule({
-        label: "Game of life simulation shader",
-        code: computeShader
-    });
-
-    // Create a compute pipeline that updates the game state.
-    const simulationPipeline = device.createComputePipeline({
-        label: "Simulation pipeline",
-        layout: pipelineLayout,
-        compute: {
-            module: simulationShaderModule,
-            entryPoint: "computeMain",
-        }
-    });
-
-    // Create a uniform buffer that describes the grid.
-    const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
-    const uniformBuffer = device.createBuffer({
-        label: "Grid Uniforms",
-        size: uniformArray.byteLength,
-        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-    });
-    device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
-
-    // Create an array representing the active state of each cell.
-    const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
-
-    // Create two storage buffers to hold the cell state.
-    const cellStateStorage = [
-        device.createBuffer({
-            label: "Cell State A",
-            size: cellStateArray.byteLength,
-            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-        }),
-        device.createBuffer({
-            label: "Cell State B",
-            size: cellStateArray.byteLength,
-            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-        })
-    ];
-
-    // Set each cell to a random state, then copy the JavaScript array into
-    // the storage buffer.
-    for (let i = 0; i < cellStateArray.length; ++i) {
-        cellStateArray[i] = Math.random() > 0.6 ? 1 : 0;
-    }
-    device.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray);
-
-
-    const bindGroups = [
-        device.createBindGroup({
-            label: "Cell renderer bind group A",
-            layout: bindGroupLayout,
-            entries: [{
-                binding: 0,
-                resource: { buffer: uniformBuffer }
-            }, {
-                binding: 1,
-                resource: { buffer: cellStateStorage[0] }
-            }, {
-                binding: 2,
-                resource: { buffer: cellStateStorage[1] }
-            }],
-        }),
-        device.createBindGroup({
-            label: "Cell renderer bind group B",
-            layout: bindGroupLayout,
-            entries: [{
-                binding: 0,
-                resource: { buffer: uniformBuffer }
-            }, {
-                binding: 1,
-                resource: { buffer: cellStateStorage[1] }
-            }, {
-                binding: 2,
-                resource: { buffer: cellStateStorage[0] }
-            }],
-        })
-    ];
-
-    let step = 0;
-    function updateGrid() {
-
-        if (context == null){
-            throw new Error("Context is null");
-        }
-
-        const encoder = device.createCommandEncoder();
-
-        const computePass = encoder.beginComputePass();
-
-        computePass.setPipeline(simulationPipeline);
-        computePass.setBindGroup(0, bindGroups[step % 2]);
-
-        const workgroupCount = Math.ceil(GRID_SIZE / WORKGROUP_SIZSE);
-        computePass.dispatchWorkgroups(workgroupCount, workgroupCount);
-
-        computePass.end();
-
-        step++;
-
-        const pass = encoder.beginRenderPass({
-            colorAttachments: [{
-                view: context.getCurrentTexture().createView(),
-                loadOp: "clear",
-                clearValue: { r: 0, g: 0.1, b: 0.7, a: 1 },
-                storeOp: "store",
-            }]
-        });
-
-        pass.setPipeline(cellPipeline);
-        pass.setVertexBuffer(0, vertexBuffer);
-        pass.setBindGroup(0, bindGroups[step % 2]);
-        pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE); // 6 vertices
-
-        pass.end();
-        // Finish the command buffer and immediately submit it.
-        device.queue.submit([encoder.finish()]);
-    }
-
-    setInterval(updateGrid, UPDATE_INTERVAL);
+if (device == null){
+    throw new Error("Device not found")
 }
 
+const context = canvas.getContext('webgpu') as GPUCanvasContext;
 
+const devicePixelRatio = window.devicePixelRatio;
+canvas.width = canvas.clientWidth * devicePixelRatio;
+canvas.height = canvas.clientHeight * devicePixelRatio;
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 
+context.configure({
+  device,
+  format: presentationFormat,
+  alphaMode: 'premultiplied',
+});
+
+// Create a vertex buffer from the cube data.
+const verticesBuffer = device.createBuffer({
+  size: cubeVertexArray.byteLength,
+  usage: GPUBufferUsage.VERTEX,
+  mappedAtCreation: true,
+});
+new Float32Array(verticesBuffer.getMappedRange()).set(cubeVertexArray);
+verticesBuffer.unmap();
+
+const pipeline = device.createRenderPipeline({
+  layout: 'auto',
+  vertex: {
+    module: device.createShaderModule({
+      code: vertexShader,
+    }),
+    buffers: [
+      {
+        arrayStride: cubeVertexSize,
+        attributes: [
+          {
+            // position
+            shaderLocation: 0,
+            offset: cubePositionOffset,
+            format: 'float32x4',
+          },
+          {
+            // uv
+            shaderLocation: 1,
+            offset: cubeUVOffset,
+            format: 'float32x2',
+          },
+        ],
+      },
+    ],
+  },
+  fragment: {
+    module: device.createShaderModule({
+      code: fragmentShader,
+    }),
+    targets: [
+      {
+        format: presentationFormat,
+      },
+    ],
+  },
+  primitive: {
+    topology: 'triangle-list',
+
+    // Backface culling since the cube is solid piece of geometry.
+    // Faces pointing away from the camera will be occluded by faces
+    // pointing toward the camera.
+    cullMode: 'back',
+  },
+
+  // Enable depth testing so that the fragment closest to the camera
+  // is rendered in front.
+  depthStencil: {
+    depthWriteEnabled: true,
+    depthCompare: 'less',
+    format: 'depth24plus',
+  },
+});
+
+const depthTexture = device.createTexture({
+  size: [canvas.width, canvas.height],
+  format: 'depth24plus',
+  usage: GPUTextureUsage.RENDER_ATTACHMENT,
+});
+
+const uniformBufferSize = 4 * 16; // 4x4 matrix
+const uniformBuffer = device.createBuffer({
+  size: uniformBufferSize,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+
+const uniformBindGroup = device.createBindGroup({
+  layout: pipeline.getBindGroupLayout(0),
+  entries: [
+    {
+      binding: 0,
+      resource: {
+        buffer: uniformBuffer,
+      },
+    },
+  ],
+});
+
+const colorTexture = device.createTexture({
+    size: [canvas.width, canvas.height],
+    format: 'rgba8unorm',
+    usage: GPUTextureUsage.RENDER_ATTACHMENT,
+  });
+
+  const colorTextureView = colorTexture.createView();
+
+const renderPassDescriptor: GPURenderPassDescriptor = {
+  colorAttachments: [
+    {
+      view: colorTextureView, // Assigned later
+
+      clearValue: [0.5, 0.5, 0.5, 1.0],
+      loadOp: 'clear',
+      storeOp: 'store',
+    },
+  ],
+  depthStencilAttachment: {
+    view: depthTexture.createView(),
+
+    depthClearValue: 1.0,
+    depthLoadOp: 'clear',
+    depthStoreOp: 'store',
+  },
+};
+
+const aspect = canvas.width / canvas.height;
+const projectionMatrix = mat4.perspective((2 * Math.PI) / 5, aspect, 1, 100.0);
+const modelViewProjectionMatrix = mat4.create();
+
+function getTransformationMatrix() {
+  const viewMatrix = mat4.identity();
+  mat4.translate(viewMatrix, vec3.fromValues(0, 0, -4), viewMatrix);
+  const now = Date.now() / 1000;
+  mat4.rotate(
+    viewMatrix,
+    vec3.fromValues(Math.sin(now), Math.cos(now), 0),
+    1,
+    viewMatrix
+  );
+
+  mat4.multiply(projectionMatrix, viewMatrix, modelViewProjectionMatrix);
+
+  return modelViewProjectionMatrix;
+}
+
+function frame() {
+  const transformationMatrix = getTransformationMatrix();
+  device?.queue.writeBuffer(
+    uniformBuffer,
+    0,
+    transformationMatrix.buffer,
+    transformationMatrix.byteOffset,
+    transformationMatrix.byteLength
+  );
+  if(renderPassDescriptor.colorAttachments)
+  renderPassDescriptor.colorAttachments[0].view = context
+    .getCurrentTexture()
+    .createView();
+
+  const commandEncoder = device?.createCommandEncoder();
+  const passEncoder = commandEncoder?.beginRenderPass(renderPassDescriptor);
+  passEncoder?.setPipeline(pipeline);
+  passEncoder?.setBindGroup(0, uniformBindGroup);
+  passEncoder?.setVertexBuffer(0, verticesBuffer);
+  passEncoder?.draw(cubeVertexCount);
+  passEncoder?.end();
+  device?.queue.submit([commandEncoder!.finish()]);
+
+  requestAnimationFrame(frame);
+}
+requestAnimationFrame(frame);
